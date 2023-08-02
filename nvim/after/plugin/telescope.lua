f202467aae0bd36f395e2da6f9e0add4fe464527
Radamés <r.alfaro8@icloud.com>
2023-08-01 20:53:49 -0400

    Add keymaps for registers & keymaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,10 +1,9 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.git_files, {})
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
---vim.keymap.set('n', '<leader>/', function()
---	builtin.grep_string({ search = vim.fn.input("Grep > ") });
---end)
+vim.keymap.set('n', '<leader>r', builtin.registers, {})
+vim.keymap.set('n', '<leader>k', builtin.keymaps, {})
 
