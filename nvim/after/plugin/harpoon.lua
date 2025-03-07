ccb3e92bbf0b3f798abd4a8241186f0d9ede9ffb
Radamés Vega-Alfaro <kaitachi@me.com>
2025-03-07 19:33:54 -0400

    feat: Add more useful keybinds for Harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -9,3 +9,8 @@ vim.keymap.set("n", "<C-g>", function() harpoon:list():select(vim.v.count1) end,
 vim.keymap.set("n", "<C-e>", function() vim.cmd [[ Telescope harpoon marks ]] end,
 	{ desc = "Open harpoon window" })
 
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
--- nvim/lua/kaitachi/remap.lua
+++ nvim/lua/kaitachi/remap.lua
@@ -18,8 +18,8 @@ vim.keymap.set("v", "<PageDown>", "<nop>")
 vim.keymap.set("n", "<leader>=", vim.cmd.Ex)
 
 -- Navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
