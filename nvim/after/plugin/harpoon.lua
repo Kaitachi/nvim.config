877e4aa7284c8e3f8a46900fb883091fea869ab2
Radamés Vega-Alfaro <kaitachi@me.com>
2025-03-10 21:06:29 -0400

    fix: Resolve issues with Harpoon & Telescope integrations =D

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -10,70 +10,50 @@ vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, { desc = "
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, { desc = "Go to fourth Harpoon file" })
 
 -- FIX: BROKEN COMMANDS! Find out how to fix these...
--- vim.keymap.set("n", "<Tab>", function() require("harpoon.ui").nav_next() end, { desc = "Go to next Harpoon file" })
--- vim.keymap.set("n", "<S-Tab>", function() require("harpoon.ui").nav_prev() end, { desc = "Go to previous Harpoon file" })
+vim.keymap.set("n", "<Tab>", function() harpoon:list():next() end, { desc = "Go to next Harpoon file" })
+vim.keymap.set("n", "<S-Tab>", function() harpoon:list():prev() end, { desc = "Go to previous Harpoon file" })
 
-vim.keymap.set("n", "<C-t>", function() vim.cmd [[ lua require("harpoon.ui").toggle_quick_menu() ]] end, { desc = "Add file to Harpoon list" })
 vim.keymap.set("n", "<C-s>", function() harpoon:list():add() end, { desc = "Add file to Harpoon list" })
 vim.keymap.set("n", "<C-g>", function() harpoon:list():select(vim.v.count1) end, { desc = "Go to selected Harpoon file" })
-vim.keymap.set("n", "<C-e>", function() vim.cmd [[ Telescope harpoon marks ]] end,
-	{ desc = "Open harpoon window" })
 
-function Remove(item)
-  item = item or harpoon:list().config.create_list_item(harpoon:list().config)
-  print("Hello")
-  local Extensions = require("harpoon.extensions")
-  local Logger = require("harpoon.logger")
-
-  local items = harpoon:list().items
-  if item ~= nil then
-    for i = 1, harpoon:list()._length do
-      local v = items[i]
-      print(vim.inspect(v))
-      if harpoon:list().config.equals(v, item) then
-        -- this clears list somehow
-        -- items[i] = nil
-        table.remove(items, i)
-        harpoon:list()._length = harpoon:list()._length - 1
-
-        Logger:log("HarpoonList:remove", { item = item, index = i })
-
-        Extensions.extensions:emit(
-          Extensions.event_names.REMOVE,
-          { list = harpoon:list(), item = item, idx = i }
-        )
-        break
-      end
-    end
-  end
+-- Telescope Integration
+local function harpoon_get_paths(files)
+	-- NOTE: This function REMOVES entries completely!
+	-- Default behavior consists of setting the element nil, but that leaves
+	-- an empty space in our list (which feels odd to me).
+	-- See the following for more details:
+	-- https://github.com/ThePrimeagen/harpoon/pull/628
+	local paths = {}
+	for _, item in ipairs(files.items) do
+		table.insert(paths, item.value)
+	end
+	return paths
 end
 
--- Telescope configuration
+local function harpoon_make_finder(paths)
+	return require("telescope.finders").new_table({
+		results = paths,
+	})
+end
+
 local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
+function toggle_telescope(harpoon_files)
+	local file_paths = harpoon_get_paths(harpoon_files)
 
 	require("telescope.pickers").new({}, {
-		prompt_title = "Harpoon",
-		finder = require("telescope.finders").new_table({
-			results = file_paths,
-		}),
+		prompt_title = "Harpoon Marks",
+		finder = harpoon_make_finder(file_paths),
 		previewer = conf.file_previewer({}),
 		sorter = conf.generic_sorter({}),
 		attach_mappings = function(prompt_buffer_number, map)
-		  map(
-			"i",
-			"<C-x>", -- your mapping here
-			function()
+		  map("i", "<C-d>", function()
 			  local state = require("telescope.actions.state")
 			  local selected_entry = state.get_selected_entry()
 			  local current_picker = state.get_current_picker(prompt_buffer_number)
 
-			  harpoon:list():remove(selected_entry.index)
-			  current_picker:refresh(make_finder())
+			  -- harpoon:list():remove_at(selected_entry.index)
+			  table.remove(harpoon:list().items, selected_entry.index)
+			  current_picker:refresh(harpoon_make_finder(harpoon_get_paths(harpoon:list())))
 			end
 		  )
 
@@ -82,6 +62,6 @@ local function toggle_telescope(harpoon_files)
   }):find()
 end
 
--- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
--- 	{ desc = "Open harpoon window" })
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+	{ desc = "Open harpoon window" })
 
