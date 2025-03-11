cbf74dbb75f77b298bd6e3e22e3c3d8c7b7893e6
Radamés Vega-Alfaro <kaitachi@me.com>
2025-03-10 21:07:11 -0400

    fix: Correctly set highlights for Git diffs

--- nvim/after/plugin/git.lua
+++ nvim/after/plugin/git.lua
@@ -7,3 +7,9 @@ vim.opt.updatetime = 100
 gitsigns.setup {
 	numhl = true
 }
+
+-- Set fancy colors
+vim.api.nvim_set_hl(0, "DiffAdded", { ctermbg = "DarkGreen", ctermfg = "Black", bg = "#008800", fg = "#000000" })
+vim.api.nvim_set_hl(0, "DiffChange", { ctermbg = "Yellow", ctermfg = "Black", bg = "#f4c900", fg = "#000000" })
+vim.api.nvim_set_hl(0, "DiffRemoved", { ctermbg = "DarkRed", ctermfg = "Black", bg = "#b90000", fg = "#000000" })
+vim.api.nvim_set_hl(0, "DiffLine", { ctermbg = "Blue", ctermfg = "White", bg = "#0000bd", fg = "#ffffff" })
