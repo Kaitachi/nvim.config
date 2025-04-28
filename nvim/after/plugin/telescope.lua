44ee4c219198b350536bd1498fefa5e37c344386
Radamés Vega-Alfaro <kaitachi@me.com>
2025-04-27 21:47:45 -0400

    feat: Telescope toggle preview

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,6 +3,11 @@ local builtin = require('telescope.builtin')
 
 telescope.setup({
 	defaults = {
+		mappings = {
+			i = {
+				['<C-p>'] = require('telescope.actions.layout').toggle_preview
+			}
+		},
 		file_ignore_patterns = {
 			"node_modules",
 			"package%-lock.json",
