83e710b37172445dd9230cd84baa11b6dfd7c11e
Radamés Vega-Alfaro <kaitachi@me.com>
2025-04-28 21:34:04 -0400

    fix: Reassign some mappings (due to macOS limitations...)
    Apparently, it's impossible to use F21-F24 on macOS.

--- nvim/after/plugin/overseer.lua
+++ nvim/after/plugin/overseer.lua
@@ -3,4 +3,4 @@ local overseer = require("overseer")
 overseer.setup()
 
 vim.keymap.set('n', '<F6>', overseer.toggle, { desc = "Toggle Overseer"})
-vim.keymap.set('n', '<F18>', overseer.run_template, { desc = "Run Overseer template" })
+vim.keymap.set('n', '<F11>', overseer.run_template, { desc = "Run Overseer template" })
