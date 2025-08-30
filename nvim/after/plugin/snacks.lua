979da5336763f95a3c8e86ed99c49594521fad7a
Radamés Vega-Alfaro <kaitachi@me.com>
2025-08-30 19:53:03 -0400

    fix: lazygit/lazydocker enhancements

--- nvim/after/plugin/snacks.lua
+++ nvim/after/plugin/snacks.lua
@@ -10,3 +10,6 @@ snacks.setup({
 -- lazygit config
 vim.keymap.set('n', '<F10>', snacks.lazygit.open, { desc = "[Snacks] Open LazyGit" })
 vim.keymap.set('n', '<F12>', snacks.lazygit.log_file, { desc = "[Snacks] Open LazyGit Log File" })
+
+-- terminal config
+vim.keymap.set('n', '<F7>', function() snacks.terminal("lazydocker") end, { desc = "[Snacks] Open LazyDocker" })
