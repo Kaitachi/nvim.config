4237dd1902832323e35559c6ab3c756dd130b0fd
Radamés Vega-Alfaro <kaitachi@me.com>
2025-08-29 20:38:42 -0400

    fix: Color overhaul

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -32,10 +32,10 @@ function ColorMyPencils(color)
 	}
 
 	-- Set color scheme options
-	vim.g["edge_style"] = 'neon'
-	vim.g["edge_diagnostic_virtual_text"] = 'highlighted'
-	vim.g["edge_menu_selection_background"] = 'green'
-	vim.g["edge_float_style"] = 'dim'
+	-- vim.g["edge_style"] = 'neon'
+	-- vim.g["edge_diagnostic_virtual_text"] = 'highlighted'
+	-- vim.g["edge_menu_selection_background"] = 'green'
+	-- vim.g["edge_float_style"] = 'dim'
 
 	color = color or "nord"
 
@@ -47,79 +47,214 @@ function ColorMyPencils(color)
 	-- Use `:colorscheme` to show current color scheme
 	vim.cmd.colorscheme(color)
 
+	local nord = vim.fn.NordPalette()
+
+	-- Mapping from Nord color to term (ansi) color
+	local nord_term = {
+
+		-- FIRST ROW
+		nord1 = 0, -- GRAYSCALE[2nd, darker]
+		nord11 = 1, -- RED
+		nord14 = 2, -- GREEN
+		nord13 = 3, -- YELLOW
+		nord9 = 4, -- BLUESCALE[3rd, pale]
+		nord15 = 5, -- PURPLE
+		nord8 = 6, -- BLUESCALE[2nd, greenish?]
+		nord5 = 7, -- WHITESCALE[2nd, mid]
+
+		-- SECOND ROW
+		nord3 = 8, -- GRAYSCALE[4th, lightest]
+		nord12 = 15, -- ORANGE
+	}
+
+	-- Mapping from ansi color positions to nord/nvim colors
+	local ansi_colors = {
+		[0] = "nord1",
+		[1] = "nord11",
+		[2] = "nord14",
+		[3] = "nord13",
+		[4] = "nord9",
+		[5] = "nord15",
+		[6] = "nord8",
+		[7] = "nord5",
+		[8] = "nord3",
+		[9] = "NvimDarkRed",
+		[10] = "NvimDarkGreen",
+		[11] = "NvimDarkYellow",
+		[12] = "NvimDarkBlue",
+		[13] = "NvimDarkMagenta",
+		[14] = "NvimDarkCyan",
+		[15] = "nord12",
+	}
+
+	if color == "nord" then
+		-- For ease of use, let's configure Nord colors to be available
+		-- as highlight groups we can easily reference
+
+		for key, value in pairs(nord) do
+			-- Set nord color highlight groups
+			vim.api.nvim_set_hl(0, key .. "_bg", { bg = value, ctermbg = (nord_term[key] or "none") })
+			vim.api.nvim_set_hl(0, key .. "_fg", { fg = value, ctermfg = (nord_term[key] or "none") })
+		end
+
+		for i = 0, 15 do
+			-- Set ansi color highlight groups
+			vim.api.nvim_set_hl(0, "ansi" .. i .. "_bg",
+				{ bg = (nord[ansi_colors[i]] or ansi_colors[i]), ctermbg = i })
+			vim.api.nvim_set_hl(0, "ansi" .. i .. "_fg",
+				{ fg = (nord[ansi_colors[i]] or ansi_colors[i]), ctermfg = i })
+		end
+	end
+
+	-- Set some convenience highlights
+	vim.api.nvim_set_hl(0, "Italic", { italic = true })
+
+	-- Merges n tables onto the target table
+	-- target is passed by ref, but is also returned (if desired)
+	local function merge(target, ...)
+		for _, other in ipairs({ ... }) do
+			for key, value in pairs(other) do
+				target[key] = value
+			end
+		end
+		return target
+	end
+
+	-- Combines n highlights onto the target `hi`
+	local function combine_hl(hi, ...)
+		local target_hl = vim.api.nvim_get_hl(0, { name = hi })
+		while target_hl["link"] do
+			-- Determine if our base highlight is a link
+			target_hl = vim.api.nvim_get_hl(0, { name = target_hl["link"] })
+		end
+
+		for _, other in ipairs({ ... }) do
+			merge(target_hl, vim.api.nvim_get_hl(0, { name = other }))
+		end
+
+		return target_hl
+	end
+
+	-- Assigns to target `hi` the combination of vararg highlights
+	local function merge_hl(hi, ...)
+		vim.api.nvim_set_hl(0, hi, combine_hl(hi, ...))
+	end
+
+	--
+	local function set_hl(hi, tbl)
+		local result = combine_hl("arst", unpack(tbl))
+		vim.api.nvim_set_hl(0, hi, result)
+	end
 
 	-- Show whitespaces without background colors
-	vim.api.nvim_set_hl(0, "NonText", { bg = "none", ctermfg = 237 })
+	vim.api.nvim_set_hl(0, "NonText", { ctermfg = 237 })
 
 	-- Distinctive EOB
-	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", ctermfg = 202 })
+	vim.api.nvim_set_hl(0, "EndOfBuffer", { ctermfg = 202 })
+
 
 
 	-- #region Custom colors for diagnostics
-	local hl_diag_error = vim.api.nvim_get_hl(0, { name = "DiagnosticError" })
-	hl_diag_error["italic"] = true
-	hl_diag_error["ctermbg"] = 9
-	hl_diag_error["ctermfg"] = 203
-	hl_diag_error["bg"] = "NvimDarkRed"
-	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", hl_diag_error)
+	merge_hl("DiagnosticVirtualTextError", "italic", "ansi9_bg")
+	merge_hl("DiagnosticVirtualTextWarn", "italic", "ansi11_bg")
+	merge_hl("DiagnosticVirtualTextInfo", "italic", "ansi14_bg")
+	merge_hl("DiagnosticVirtualTextHint", "italic", "ansi12_bg")
+	merge_hl("DiagnosticVirtualTextOk", "italic", "ansi10_bg")
 
-	local hl_diag_warn = vim.api.nvim_get_hl(0, { name = "DiagnosticWarn" })
-	hl_diag_warn["italic"] = true
-	hl_diag_warn["ctermbg"] = 11
-	hl_diag_warn["ctermfg"] = 15
-	hl_diag_warn["bg"] = "NvimDarkYellow"
-	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", hl_diag_warn)
-
-	local hl_diag_info = vim.api.nvim_get_hl(0, { name = "DiagnosticInfo" })
-	hl_diag_info["italic"] = true
-	hl_diag_info["ctermbg"] = 14
-	hl_diag_info["bg"] = "NvimDarkCyan"
-	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", hl_diag_info)
-
-	local hl_diag_hint = vim.api.nvim_get_hl(0, { name = "DiagnosticHint" })
-	hl_diag_hint["italic"] = true
-	hl_diag_hint["ctermbg"] = 12
-	hl_diag_hint["ctermfg"] = 15
-	hl_diag_hint["bg"] = "NvimDarkBlue"
-	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", hl_diag_hint)
-
-	local hl_diag_ok = vim.api.nvim_get_hl(0, { name = "DiagnosticOk" })
-	hl_diag_ok["italic"] = true
-	hl_diag_ok["ctermbg"] = 10
-	hl_diag_ok["bg"] = "NvimDarkGreen"
-	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextOk", hl_diag_ok)
+	-- Code snippet to test some virtual text colors
+	-- local target_hl = vim.api.nvim_get_hl(0, { name = "test" })
+	-- vim.api.nvim_set_hl(0, "NonText", target_hl)
+	-- arst
+	-- local arst
 	-- #endregion
 
 	-- # region Overwrite more general highlights
-	local nord = vim.fn.NordPalette()
-
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = nord["nord1"], ctermbg = 0 })
-	vim.api.nvim_set_hl(0, "Visual", { bg = "NvimDarkGreen", ctermbg = 10 })
-	vim.api.nvim_set_hl(0, "Search", { bg = "NvimDarkGrey2", ctermbg = 237 })
-	vim.api.nvim_set_hl(0, "CurSearch", { bg = "NvimDarkYellow", ctermbg = 11 })
+	set_hl("NormalFloat", { "nord1_bg" })
+	set_hl("Visual", { "ansi10_bg" })
+	set_hl("Search", { "ansi8_bg" })
+	set_hl("CurSearch", { "ansi11_bg" })
+	set_hl("IncSearch", { "italic", "ansi10_bg" })
+	set_hl("LineNr", { "nord1_bg" })
+	set_hl("SignColumn", { "nord1_bg" })
 	-- #endregion
 
 	-- #region Telescope Colors
-	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = nord["nord1"], fg = nord["nord6"], ctermbg = 0 })
-	vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = nord["nord2"], ctermbg = 8 })
-	vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = nord["nord8"] })
-	vim.api.nvim_set_hl(0, "TelescopePromptCounter", { fg = nord["nord14"] })
-	vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = nord["nord7"] })
-	vim.api.nvim_set_hl(0, "TelescopeResultsIdentifier", { fg = nord["nord12"] })
-	vim.api.nvim_set_hl(0, "TelescopeResultsLineNr", { fg = nord["nord7"] })
-	vim.api.nvim_set_hl(0, "TelescopeResultsNumber", { fg = nord["nord12"] })
-	vim.api.nvim_set_hl(0, "TelescopeResultsComment", { fg = nord["nord6"] })
+	set_hl("TelescopeNormal", { "nord1_bg", "nord6_fg" })
+	set_hl("TelescopeSelection", { "ansi10_bg" })
+	set_hl("TelescopeTitle", { "nord8_fg" })
+	set_hl("TelescopePromptCounter", { "nord14_fg" })
+	set_hl("TelescopeMatching", { "nord7_fg" })
+	set_hl("TelescopeResultsIdentifier", { "nord12_fg" })
+	set_hl("TelescopeResultsLineNr", { "nord7_fg" })
+	set_hl("TelescopeResultsNumber", { "nord12_fg" })
+	set_hl("TelescopeResultsComment", { "nord6_fg" })
 	-- #endregion
 
 	-- #region Which-Key Colors
-	vim.api.nvim_set_hl(0, "WhichKeyTitle", { bg = nord["nord1"], fg = nord["nord4"] })
-	vim.api.nvim_set_hl(0, "WhichKey", { fg = nord["nord6"] })
-	vim.api.nvim_set_hl(0, "WhichKeyIconOrange", { fg = nord["nord12"] })
+	set_hl("WhichKeyTitle", { "nord1_bg", "nord4_fg" })
+	set_hl("WhichKey", { "nord6_fg" })
+	set_hl("WhichKeyIconOrange", { "nord12_fg" })
 	-- #endregion
 
 	-- #region AI Tool Colors
-	vim.api.nvim_set_hl(0, "AugmentSuggestionHighlight",
-		{ bg = "NvimDarkMagenta", fg = nord["nord15"], ctermbg = 13, ctermfg = 5, italic = true })
+	set_hl("AISuggestionHighlight", { "nord15_fg", "ansi13_bg", "italic" })
+	-- #endregion
+
+	-- #region Treesitter Colors
+	-- nvim-treesitter-context
+	set_hl("TreesitterContext", { "nord3_bg" })
+	set_hl("TreesitterContextLineNumber", { "nord4_bg", "nord2_fg" })
+	-- #endregion
+
+	-- #region Todo Colors
+	-- Red
+	-- FIX: sample fix string
+	set_hl("TodoSignFIX", { "nord11_fg" })
+	set_hl("TodoFgFIX", { "nord11_fg" })
+	set_hl("TodoBgFIX", { "bold", "nord11_bg", "nord6_fg" })
+
+	-- Orange
+	-- TODO: sample todo string
+	set_hl("TodoSignTODO", { "nord12_fg" })
+	set_hl("TodoFgTODO", { "nord12_fg" })
+	set_hl("TodoBgTODO", { "bold", "nord12_bg", "nord6_fg" })
+
+	-- Yellow
+	-- WARN: sample warn string
+	set_hl("TodoSignWARN", { "nord13_fg" })
+	set_hl("TodoFgWARN", { "nord13_fg" })
+	set_hl("TodoBgWARN", { "bold", "nord13_bg", "nord6_fg" })
+
+	-- Green
+	-- NOTE: sample note string
+	-- INFO: sample info string
+	set_hl("TodoSignNOTE", { "nord14_fg" })
+	set_hl("TodoFgNOTE", { "nord14_fg" })
+	set_hl("TodoBgNOTE", { "bold", "nord14_bg", "nord6_fg" })
+
+	-- Cyan
+	-- MARK: sample mark string
+	set_hl("TodoSignMARK", { "nord8_fg" })
+	set_hl("TodoFgMARK", { "nord8_fg" })
+	set_hl("TodoBgMARK", { "bold", "nord8_bg", "nord6_fg" })
+
+	-- Blue
+	-- HACK: sample hack string
+	set_hl("TodoSignHACK", { "nord10_fg" })
+	set_hl("TodoFgHACK", { "nord10_fg" })
+	set_hl("TodoBgHACK", { "bold", "nord10_bg", "nord6_fg" })
+
+	-- Violet
+	-- PERF: sample perf string
+	set_hl("TodoSignPERF", { "nord15_fg" })
+	set_hl("TodoFgPERF", { "nord15_fg" })
+	set_hl("TodoBgPERF", { "bold", "nord15_bg", "nord6_fg" })
+
+	-- Gray
+	-- TEST: sample test string
+	set_hl("TodoSignTEST", { "nord5_fg" })
+	set_hl("TodoFgTEST", { "nord5_fg" })
+	set_hl("TodoBgTEST", { "bold", "nord5_bg", "nord3_fg" })
 	-- #endregion
 end
 
