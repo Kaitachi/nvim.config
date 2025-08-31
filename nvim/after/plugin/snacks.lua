b89edb8f56f1fc7922279a9fb7de028e15767a98
Radamés Vega-Alfaro <kaitachi@me.com>
2025-08-31 18:31:51 -0400

    fix: fancy virtual terminal colors

--- nvim/after/plugin/snacks.lua
+++ nvim/after/plugin/snacks.lua
@@ -7,9 +7,17 @@ snacks.setup({
 	lazygit = {},
 })
 
+-- default highlight: NormalFloat
+
 -- lazygit config
-vim.keymap.set('n', '<F10>', snacks.lazygit.open, { desc = "[Snacks] Open LazyGit" })
+vim.keymap.set('n', '<F10>', function()
+	vim.api.nvim_set_hl(0, "SnacksNormal", { link = "NormalFloat" })
+	snacks.lazygit.open(lazygit_opts)
+end, { desc = "[Snacks] Open LazyGit" })
 vim.keymap.set('n', '<F12>', snacks.lazygit.log_file, { desc = "[Snacks] Open LazyGit Log File" })
 
 -- terminal config
-vim.keymap.set('n', '<F7>', function() snacks.terminal("lazydocker") end, { desc = "[Snacks] Open LazyDocker" })
+vim.keymap.set('n', '<F7>', function()
+	vim.api.nvim_set_hl(0, "SnacksNormal", { link = "ansi12_bg" })
+	snacks.terminal("lazydocker")
+end, { desc = "[Snacks] Open LazyDocker" })
