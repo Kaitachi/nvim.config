984b3cc523279e080d5d5c13248c19a081e69352
Radamés Vega-Alfaro <kaitachi@me.com>
2025-09-01 19:45:46 -0400

    chore: Gut out most Telescope usages

--- nvim/after/plugin/snacks.lua
+++ nvim/after/plugin/snacks.lua
@@ -9,15 +9,40 @@ snacks.setup({
 
 -- default highlight: NormalFloat
 
--- lazygit config
+--#region lazygit config
 vim.keymap.set('n', '<F10>', function()
 	vim.api.nvim_set_hl(0, "SnacksNormal", { link = "NormalFloat" })
 	snacks.lazygit.open()
 end, { desc = "[Snacks] Open LazyGit" })
 vim.keymap.set('n', '<F12>', snacks.lazygit.log_file, { desc = "[Snacks] Open LazyGit Log File" })
+--#endregion
 
--- terminal config
+
+--#region terminal config
 vim.keymap.set('n', '<F7>', function()
 	vim.api.nvim_set_hl(0, "SnacksNormal", { link = "ansi12_bg" })
 	snacks.terminal("lazydocker")
 end, { desc = "[Snacks] Open LazyDocker" })
+--#endregion
+
+
+--#region picker config
+-- Snacks Search
+vim.keymap.set('n', '<leader><leader>', snacks.picker.pickers, { desc = "[Snacks] Show Pickers" })
+
+-- Vim Search
+vim.keymap.set('n', '<leader>h', snacks.picker.help, { desc = "[Snacks] Search Help" })
+vim.keymap.set('n', '<leader>k', snacks.picker.keymaps, { desc = "[Snacks] Show Vim Keymaps" })
+vim.keymap.set('n', '<leader>m', snacks.picker.marks, { desc = "[Snacks] Show Vim Marks" })
+vim.keymap.set('n', '<leader>r', snacks.picker.registers, { desc = "[Snacks] Show Vim Registers" })
+vim.keymap.set('n', '<leader>b', snacks.picker.buffers, { desc = "[Snacks] Show Vim Buffers" })
+vim.keymap.set('n', '<leader>c', snacks.picker.commands, { desc = "[Snacks] Show Vim Commands" })
+
+-- File Search
+vim.keymap.set('n', '<leader>=', snacks.picker.files, { desc = "[Snacks] Find Files" })
+vim.keymap.set('n', '+', snacks.picker.smart, { desc = "[Snacks] Smart Find Files" })
+-- TODO: Cannot fuzzy-find
+vim.keymap.set('n', '<leader>f', function()
+	snacks.picker.grep({ live = true })
+end, { desc = "[Snacks] Fuzzy-Find Text" })
+--#endregion
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local telescope = require("telescope")
-local builtin = require('telescope.builtin')
-local strings = require('plenary.strings')
+-- local builtin = require('telescope.builtin')
+-- local strings = require("plenary.strings")
 
 telescope.setup({
 	defaults = {
@@ -15,83 +15,6 @@ telescope.setup({
 			".git"
 		}
 	},
-	pickers = {
-		oldfiles = {
-			cwd_only = true,
-		}
-	},
-	extensions = {
-		["frecency"] = {
-			default_workspace = "CWD",
-			show_scores = true,
-			show_unindexed = true,
-			matcher = "fuzzy",
-			db_version = "v2",
-		},
-		["ui-select"] = {
-			require("telescope.themes").get_cursor {
-				-- even more opts
-			},
-		}
-	}
 })
 
--- function DeepPrint(t)
--- 	local request_headers_all = ""
--- 	for k, v in pairs(t) do
--- 		if type(v) == "table" then
--- 			request_headers_all = request_headers_all .. "[" .. k .. " " .. DeepPrint(v) .. "] "
--- 		else
--- 			local rowtext = ""
--- 			if type(k) == "string" then
--- 				rowtext = string.format("[%s %s] ", k, v)
--- 			else
--- 				rowtext = string.format("[%s] ", v)
--- 			end
--- 			request_headers_all = request_headers_all .. rowtext
--- 		end
--- 	end
--- 	return request_headers_all
--- end
-
--- HACK: Useful to let us know what kind we want to use for ui-select options
--- vim.ui.select = function(items, opts, on_choice)
--- 	print(opts.kind)
--- 	print(DeepPrint(items))
--- 	-- print(table.concat(items, "\n"))
--- 	-- for k, v in pairs(items) do
--- 	-- 	print(k .. " = " .. v)
--- 	-- end
--- end
-
-telescope.load_extension('ui-select')
-telescope.load_extension('frecency')
 telescope.load_extension('harpoon')
-
--- Telescope Search
-vim.keymap.set('n', '<leader><leader>', builtin.builtin, { desc = "[Telescope] Show Telescope commands" })
-vim.keymap.set('n', '<leader>P', builtin.planets, { desc = "[Telescope] Planets" })
-
--- Vim Search
-vim.keymap.set('n', '<c-bslash>H', builtin.help_tags, { desc = "[Telescope] Search Help" })
-vim.keymap.set('n', '<c-bslash>k', builtin.keymaps, { desc = "[Telescope] Show Vim Keymaps" })
-vim.keymap.set('n', '<c-bslash>c', builtin.commands, { desc = "[Telescope] Show Vim Commands" })
-vim.keymap.set('n', '<c-bslash>b', builtin.buffers, { desc = "[Telescope] Show Vim Buffers" })
-vim.keymap.set('n', '<c-bslash>r', builtin.registers, { desc = "[Telescope] Show Vim Registers" })
-
--- File Search
-vim.keymap.set('n', '<leader>=', '<cmd>Telescope find_files<CR>',
-	{ desc = "[Telescope] Find files in project" })
-vim.keymap.set('n', '+', '<cmd>Telescope frecency workspace=CWD<CR>',
-	{ desc = "[Telescope] Frecency-find files in project" })
-vim.keymap.set('n', '<space>f', builtin.live_grep, { desc = "[Telescope] Fuzzy-find text" })
-
--- Buffer Search
-vim.keymap.set('n', '<leader>M', builtin.marks, { desc = "[Telescope] Show Vim Marks" })
-vim.keymap.set('n', '<space>s', function() builtin.lsp_document_symbols({ symbols = { "function" } }) end,
-	{ desc = "[Telescope] Show LSP Functions" })
-
--- LSP
-vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = "[Telescope] Show LSP References", nowait = true })
-vim.keymap.set('n', '<leader>D', builtin.lsp_type_definitions, { desc = "[Telescope] Show LSP Type Definitions" })
-vim.keymap.set('n', '<leader>W', builtin.lsp_workspace_symbols, { desc = "[Telescope] Show LSP Workspace Symbols" })
